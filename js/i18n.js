4de8fade23129a402f4a01791178eb4b14fa1df9
foreverfl <forever_fl@naver.com>
2022-09-14 22:21:18 +0900

    220914 22:21

---
+++ js/i18n.js
@@ -0,0 +1,182 @@
+// language source
+const languages = {
+    "en": {
+        en: "English",
+        ja: "Japanese",
+        ko: "Koean",
+        my_name: "My name is",
+        myeongkyu: "Myeongkyu Jeon",
+        summary: "I am a backend engineer who majored in English Language & Literature. Currently I am searching a job.",
+        resume: "My resume is here.",
+        about: "01. About Me",
+        about_detail: "  Hello. My name is Myeongkyu Jeon. I usually agonize about how I can simplify what I do and this tendency has made me be interested in programming. Currently, I am a back-end engineer, but I hope to be a full-stack engineer.\n\n  I majored in English/Japanese Language & Literature in ChungAng University and worked as an English educator for 2 years. After that, I had a military service as an Korea Air Force's officer for 3 years and 3 months. During serving in a military service, I came to be interested in programming and I have been doing programming.\n\n  Currently, I went through national IT education program, and I have been studying and doing my own project. My tech stack is like this.",
+        experience: "02. Where I've Worked",
+        development: "development",
+        not_development: "non-development",
+        rokaf: "ROKAF",
+        rokaf_career: "Operation Control Officer",
+        rokaf_at: "@ ROKAF",
+        rokaf_work_1: "Overally managed company such as gun&ammunition, supply and so on.",
+        rokaf_work_2: "Managed the troop.",
+        rokaf_work_3: "Mediated between an artillery unit and headquarters through shiftwork.",
+        messi_career: "Educator",
+        messi_at: "@ Messi",
+        messi_work_1: "Lectured on English for English evaluation test in Korea.",
+        messi_work_2: "Made a plan before a lecture.",
+        messi_work_3: "Made teaching materials for a lecture.",
+        work: "03. Some Things I’ve Built",
+        work_solo: "Personal Project",
+        work_team: "Team Project",
+        my_dictionary: "  It is a website saving words' entry and meaning to this website from dictionary sites through the chrome extension. Afterwards, I plan to make a development of app version of this website.",
+        my_tube: "  It is in the form of youtube, added simple login function.",
+    },
+
+    "ja": {
+        en: "英語",
+        ja: "日本語",
+        ko: "韓国語",
+        my_name: "私の名前は",
+        myeongkyu: "「ミョンギュウ」です。",
+        summary: "私は英語英学を専攻したバックエンドエンジニアであります。今は求職しています。",
+        resume: "履歴書はここをクリックしてください。",
+        about: "01. 自己紹介",
+        about_detail: "  初めまして。私は全銘揆(ジョンミョンギュウ)と申します。私は普段どうすれば私の作業を単純化させるかを悩みます。私のこんな性格のため、私はプログラミングに関心を持つようになりました。今はバックエンドエンジニアですけど、いつかプルスタックエンジニアになることを希望しています。\n\n  私は韓国の中央大学で英語英文学と日語日文学を専攻し、そのあと2年間英語の塾で教師として働きました。そして国の呼びに応じて空軍の士官として3年間韓国の軍隊で働きました。途中でプログラミングに興味を持たされ、今までプログラミンをしています。\n\n  今は国で実施するIT教育を終え、個人的な勉強とプロジェクトの進行中にいます。私の技術スタックは次のとおりです。",
+        experience: "02. 経歴事項",
+        development: "開発職",
+        not_development: "非開発職",
+        rokaf: "ROKAF",
+        rokaf_career: "基地中隊長/作戦統制将校",
+        rokaf_at: "@ ROKAF",
+        rokaf_work_1: "部隊の銃器弾薬、部隊の補給等の全般的な管理",
+        rokaf_work_2: "多数の兵力管理",
+        rokaf_work_3: "シフト勤務をしながら、砲台と司令部の間で架け橋の役割遂行",
+        messi_career: "英語教師",
+        messi_at: "@ Messi",
+        messi_work_1: "韓国の英語試験の備える学生たちに多数の英語の講義",
+        messi_work_2: "授業の前に講義の計画作り",
+        messi_work_3: "授業のための自体教材作り",
+        work: "03. プロジェクト",
+        work_solo: "個人プロジェクト",
+        work_team: "チームプロジェクト",
+        my_dictionary: "  Chromeの拡張機能(extension)を通じて辞書サイトたちからこのサイトに単語を集め、テストを通じて学習するウェブサイトであります。この後、アプリのバージョンを開発する予定であります。",
+        my_tube: "  ユーチューブの形のビューに簡単なログイン機能を追加しました。",
+    },
+
+    "ko": {
+        en: "영어",
+        ja: "일본어",
+        ko: "한국어",
+        my_name: "제 이름은",
+        myeongkyu: "전명규입니다.",
+        summary: "저는 영어영문학을 전공한 백엔드 엔지니어입니다. 현재는 구직 상태에 있습니다.",
+        resume: "이력서는 여기를 클릭하세요.",
+        about: "01. 자기소개",
+        about_detail: "  안녕하세요. 제 이름은 전명규라고 합니다. 저는 평소에 제가 하는 행위를 어떻게 하면 작업을 단순화시킬 수 있을지 고민을 많이 하는 편인데, 이런 성향 때문에 프로그래밍에 관심을 가지게 되었습니다. 현재는 백엔드 엔지니어지만 풀스택 엔지니어를 지향하고 있습니다.\n\n  저는 중앙대학교에서 영어영문학과 일어일문학을 전공했고 2년 동안 영어학원에서 강사로 일했습니다. 이후에 나라의 부름을 받아 공군 학사장교로 3년 3개월 동안 군복무를 수행하고, 프로그래밍에 흥미를 가지게 되어서 지금까지 이어오고 있습니다.\n\n  현재는 국비지원 it교육을 수료하고, 개인적인 공부와 프로젝트를 진행 중에 있습니다. 제 기술 스택은 다음과 같습니다.",
+        experience: "02. 경력사항",
+        development: "개발직",
+        not_development: "비개발직",
+        rokaf: "대한민국 공군",
+        rokaf_career: "기지중대장/작전통제장교",
+        rokaf_at: "@ 대한민국 공군",
+        rokaf_work_1: "부대의 총기탄약, 부대보급 등 전반적인 관리",
+        rokaf_work_2: "다수의 병력 관리",
+        rokaf_work_3: "교대근무를 진행하며 포대와 사령부에서의 중간 다리역할 수행",
+        messi_career: "전임강사",
+        messi_at: "@ Messi",
+        messi_work_1: "내신/수능/텝스 등의 다수의 영어 강의",
+        messi_work_2: "수업 전 강의 계획 수립",
+        messi_work_3: "수업을 위한 자체 교재 및 자료 제작",
+        work: "03. 프로젝트",
+        work_solo: "개인 프로젝트",
+        work_team: "팀 프로젝트",
+        my_dictionary: "  크롬 확장프로그램(extension)을 통해 사전 사이트에서 단어를 해당 사이트에 저장한 후 테스트를 통해 학습하는 웹사이트입니다. 차후에 앱 버전 개발 예정입니다.",
+        my_tube: "  유튜브 형태의 뷰에 간단한 로그인 기능을 추가했습니다.",
+
+    }
+};
+
+// i18n
+selectLanguage();
+function selectLanguage() {
+
+    const en = document.querySelector('#en');
+    const ja = document.querySelector('#ja');
+    const ko = document.querySelector('#ko');
+
+    let setLanguage = (lang) => {
+        let changeNodeLists = Array.prototype.slice.call(document.querySelectorAll('[data-detect]'));
+
+        changeNodeLists.map(value => {
+            value.textContent = languages[lang][value.dataset.detect]
+        })
+    };
+
+    en.addEventListener("click", () => {
+        setLanguage(en.dataset.lang);
+        localStorage.setItem('language', 'en');
+    });
+
+    ja.addEventListener("click", () => {
+        setLanguage(ja.dataset.lang);
+        localStorage.setItem('language', 'ja');
+    });
+
+    ko.addEventListener("click", () => {
+        setLanguage(ko.dataset.lang);
+        localStorage.setItem('language', 'ko');
+    });
+};
+
+getLanguageSetting();
+function getLanguageSetting() {
+
+    const setting = localStorage.getItem('language');
+
+    if (setting === null) {
+        const locale = getLocale();
+        if (locale === 'ko') {
+            localStorage.setItem('language', 'ko');
+        } else if (locale === 'ja') {
+            localStorage.setItem('language', 'ja');
+        } else {
+            localStorage.setItem('language', 'en');
+        }
+    }
+
+    const en = document.querySelector('#en');
+    const ja = document.querySelector('#ja');
+    const ko = document.querySelector('#ko');
+
+    let setLanguage = (lang) => {
+        let changeNodeLists = Array.prototype.slice.call(document.querySelectorAll('[data-detect]'));
+
+        changeNodeLists.map(value => {
+            value.textContent = languages[lang][value.dataset.detect]
+        })
+    };
+
+    if (setting === 'ko') {
+        setLanguage(ko.dataset.lang);
+    } else if (setting === 'ja') {
+        setLanguage(ja.dataset.lang);
+    } else {
+        setLanguage(en.dataset.lang);
+    }
+};
+
+function getLocale() {
+    if (navigator) {
+        if (navigator.language) {
+            return navigator.language;
+        }
+        else if (navigator.browserLanguage) {
+            return navigator.browserLanguage;
+        }
+        else if (navigator.systemLanguage) {
+            return navigator.systemLanguage;
+        }
+        else if (navigator.userLanguage) {
+            return navigator.userLanguage;
+        }
+    }
+}
