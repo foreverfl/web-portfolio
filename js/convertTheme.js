f68105f06aadacc910419238203eb6449c52c219
foreverfl <forever_fl@naver.com>
2022-09-16 14:35:38 +0900

    220916 14:35

--- js/convertTheme.js
+++ js/convertTheme.js
@@ -2,49 +2,64 @@
 getMode();
 function getMode() {
 
-    const btnDarkMode = document.querySelector('#btn_dark_mode');
+    const portFolioBtnModes = document.querySelectorAll('.portFolio_btn_mode');
     const mode = localStorage.getItem('mode');
 
     if (mode === 'dark') {
-        btnDarkMode.setAttribute('checked', 'true');
-        toDarkTheme();
+        for (let i = 0; i < portFolioBtnModes.length; i++) {
+            portFolioBtnModes[i].setAttribute('checked', 'true');
+            toDarkTheme();
+        }
+
     } else {
-        btnDarkMode.removeAttribute('checked');
-        toLightTheme();
+        for (let i = 0; i < portFolioBtnModes.length; i++) {
+            portFolioBtnModes[i].removeAttribute('checked');
+            toLightTheme();
+        }
     }
 }
 
 selectMode();
 function selectMode() {
 
-    const btnDarkMode = document.querySelector('#btn_dark_mode');
-    btnDarkMode.addEventListener('change', function (event) {
+    const portFolioBtnModes = document.querySelectorAll('.portFolio_btn_mode');
 
-        if (event.target.checked) {
-            localStorage.setItem('mode', 'dark');
-            toDarkTheme();
-        } else {
-            localStorage.setItem('mode', 'light');
-            toLightTheme();
-        }
-    });
+    for (let i = 0; i < portFolioBtnModes.length; i++) {
+        portFolioBtnModes[i].addEventListener('change', function (event) {
+
+            if (event.target.checked) {
+                localStorage.setItem('mode', 'dark');
+                toDarkTheme();
+            } else {
+                localStorage.setItem('mode', 'light');
+                toLightTheme();
+            }
+        });
+    }
+
 };
 
 function toDarkTheme() {
 
     let body = document.querySelector('body');
+    body.style.backgroundColor = '#181818';
+    body.style.color = 'white';
+
+    // navbar
     let portfolioNavContents = document.querySelectorAll('.portfolio_nav_content');
     let portfolioImageNouns = document.querySelectorAll('.portfolio_image_noun');
-    let navbar = document.querySelector('#navbar>header');
+    let navbars = document.querySelectorAll('#navbar>header');
+    let headerMobile = document.querySelector('#header_mobile');
 
-    body.style.backgroundColor = '#181818';
-    navbar.style.backgroundColor = '#181818';
-    body.style.color = 'white';
-    
+    for (let i = 0; i < navbars.length; i++) {
+        navbars[i].style.backgroundColor = '#181818';
+    }
 
     for (let i = 0; i < portfolioNavContents.length; i++) {
         portfolioNavContents[i].classList.replace('text-dark', 'text-light');
     }
+
+    headerMobile.classList.add('navbar-dark');
 
     // images
     const path_dark = './images/noun/dark/'
@@ -54,21 +69,47 @@ function toDarkTheme() {
         const fileName_2 = fileName_1[fileName_1.length - 1];
         portfolioImageNouns[i].setAttribute('src', path_dark + fileName_2);
     }
+
+    const pathDark = './images/noun/dark/'
+    const pathDarkHover = './images/noun/dark_hover/'
+
+    for (let i = 0; i < portfolioImageNouns.length; i++) {
+        const fileName_1 = portfolioImageNouns[i].getAttribute('src').split(/\//);
+        const fileName_2 = fileName_1[fileName_1.length - 1];
+
+        portfolioImageNouns[i].addEventListener('mouseover', function () {
+            portfolioImageNouns[i].style.width = '40px';
+            portfolioImageNouns[i].setAttribute('src', pathDarkHover + fileName_2);
+        });
+
+        portfolioImageNouns[i].addEventListener('mouseleave', function () {
+            portfolioImageNouns[i].style.width = '30px';
+            portfolioImageNouns[i].setAttribute('src', pathDark + fileName_2);
+        });
+    }
 }
 
 function toLightTheme() {
 
     let body = document.querySelector('body');
+    body.style.backgroundColor = 'white';
+    body.style.color = '#181818';
+
+    // navbar
     let portfolioNavContents = document.querySelectorAll('.portfolio_nav_content');
     let portfolioImageNouns = document.querySelectorAll('.portfolio_image_noun');
-    let navbar = document.querySelector('#navbar>header');
+    let navbars = document.querySelectorAll('#navbar>header');
+    let headerMobile = document.querySelector('#header_mobile');
 
-    body.style.backgroundColor = 'white';
-    navbar.style.backgroundColor = 'white';
-    body.style.color = '#181818';
+    for (let i = 0; i < navbars.length; i++) {
+        navbars[i].style.backgroundColor = 'white';
+    }
+
     for (let i = 0; i < portfolioNavContents.length; i++) {
         portfolioNavContents[i].classList.replace('text-light', 'text-dark');
     }
+
+    headerMobile.classList.remove('navbar-dark');
 
     // images
     const path_light = './images/noun/light/'
@@ -78,4 +119,23 @@ function toLightTheme() {
         const fileName_2 = fileName_1[fileName_1.length - 1];
         portfolioImageNouns[i].setAttribute('src', path_light + fileName_2);
     }
+
+    const pathLight = './images/noun/light/'
+    const pathLightHover = './images/noun/light_hover/'
+
+    for (let i = 0; i < portfolioImageNouns.length; i++) {
+        const fileName_1 = portfolioImageNouns[i].getAttribute('src').split(/\//);
+        const fileName_2 = fileName_1[fileName_1.length - 1];
+
+        portfolioImageNouns[i].addEventListener('mouseover', function () {
+            portfolioImageNouns[i].style.width = '40px';
+            portfolioImageNouns[i].setAttribute('src', pathLightHover + fileName_2);
+        });
+
+        portfolioImageNouns[i].addEventListener('mouseleave', function () {
+            portfolioImageNouns[i].style.width = '30px';
+            portfolioImageNouns[i].setAttribute('src', pathLight + fileName_2);
+        });
+    }
+
 }
